e34fc15396217b1bf06c6a11ab00fe830227646b
hyz1433376288 <1433376288@qq.com>
2024-09-26 11:56:00 +0800

    modify

--- task1/R-lang-learn.R
+++ task1/R-lang-learn.R
@@ -1,2 +1,19 @@
 # vector usage
-vec <- c(1:100)
+vec <- c(1:80)
+
+
+R version 4.4.1 (2024-06-14 ucrt) -- "Race for Your Life"
+Copyright (C) 2024 The R Foundation for Statistical Computing
+Platform: x86_64-w64-mingw32/x64
+
+R是自由软件，不附带任何担保。
+在某些条件下你可以将其自由分发。
+用'license()'或'licence()'来看分发的详细条件。
+
+R是个合作计划，有许多人为之做出了贡献.
+用'contributors()'来看合著者的详细情况
+用'citation()'会告诉你如何在出版物中正确地引用R或R程序包。
+
+用'demo()'来看一些示例程序，用'help()'来阅读在线帮助文件，或
+用'help.start()'通过HTML浏览器来看帮助文件。
+输入'q()'退出R.
